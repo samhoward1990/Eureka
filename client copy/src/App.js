742b72fa7b43f07c7f16df8f4e86729e07676952
nikkisowell <nicolesowell.dev@outlook.com>
2019-11-05 20:53:35 -0600

    added blog and chat folder

---
+++ client copy/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navbar from './components/navbar';
+import HomepageContent from './components/homepage-content';
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <HomepageContent />
+    </div >
+  );
+}
+
+export default App;
